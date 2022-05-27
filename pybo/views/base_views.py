9529c989ea33f8b2fc3a64d4d81eb6697f9440e4
KyungmeeKim <kkyungmee0209@gmail.com>
2022-05-27 16:15:11 +0900

    add Searching

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,5 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+
 from ..models import Question, Answer
 
 
@@ -13,11 +15,21 @@ MAX_LIST_CNT = 10
 
 def index(request):
     page = request.GET.get('page', '1') # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
+
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목
+            Q(content__icontains=kw) |  # 내용
+            Q(author__username__icontains=kw) | #질문 글쓴이
+            Q(answer__author__username__icontains=kw)   # 답변 글쓴이
+        ).distinct()
+
+
+    # 페이징 처리
     paginator = Paginator(question_list, MAX_LIST_CNT)
     page_obj = paginator.get_page(page)
-    #context = {'question_list': page_obj}
-    #return render(request, 'pybo/question_list.html', context)
     # 전체 페이지 마지막 번호
     last_page_num = 0
     for last_page in paginator.page_range:
@@ -38,6 +50,8 @@ def index(request):
         'last_page_num' : last_page_num,
         'page_start_number' : page_start_number,
         'page_end_number' : page_end_number,
+        'page' : page,
+        'kw' :kw
     }
 
     return render(request, 'pybo/question_list.html', context)
